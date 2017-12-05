721db4109e2f4efcb0c5a1d8f79f7bb22400ca1f
tintoll <tintoll@github.com>
2017-12-05 23:46:06 +0900

    설명추가

--- JPNextStep02/src/CalculatorTest.java
+++ JPNextStep02/src/CalculatorTest.java
@@ -6,7 +6,11 @@ import org.junit.Test;
 
 public class CalculatorTest {
 	
-	private Calculator cal; // JUnit에서는 전역변수에서 인스턴스를 생성하는것을 권장하지 않음.
+	// JUnit에서는 전역변수에서 인스턴스를 생성하는것을 권장하지 않음.
+	// 생성된 Calculator 인스턴스를 재사용하는 방식으로 동작하다보니 
+	// add()를 실행하면 Calculator의 상태 값이 변경되는데 sub()를 실행할때 영향을 미칠 수 가 있다.
+	// @Before를 사용해 테스트 메소드 간에 영향을 미치지 않으면서 독립적으로 테스트 메소드를 실행하기 위함이다. 
+	private Calculator cal; 
 	
 	// @Before 사용한 메서드가 @Test메서드 실행 전에 매번호출된다.
 	@Before
